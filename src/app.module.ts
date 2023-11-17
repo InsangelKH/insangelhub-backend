ec02266909c173aa237c026d3ae28f36a3127f10
Ibragim Khaibullin <khaibullin.ibr@gmail.com>
2023-11-17 12:46:39 +0600

    Fixed bug with serving static images

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,12 @@
+import { UserModule } from '@app/user/user.module';
 import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOptions } from 'db/data-source';
-import { UserModule } from '@app/user/user.module';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 import { AuthMiddleware } from './user/middleware/auth.middleware';
 
 @Module({
     imports: [
         TypeOrmModule.forRoot(dataSourceOptions),
-        ServeStaticModule.forRoot({
-            rootPath: join('C:/Users/Ibragim/Desktop/проекты/InsangelHub/insangelhub-backend/public'),
-        }),
         UserModule,
     ],
     controllers: [],
