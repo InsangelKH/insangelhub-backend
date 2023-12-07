00c51a80ed6e03939a03f69c0eae2a4dbc7f5cc4
Ibragim Khaibullin <khaibullin.ibr@gmail.com>
2023-12-07 21:56:33 +0600

    Fixed url

--- db/data-source.ts
+++ db/data-source.ts
@@ -2,7 +2,7 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 
 export const dataSourceOptions: DataSourceOptions = {
     type: 'postgres',
-    url: process.env.DATABASE_URL,
+    url: 'postgres://jobyycrbtcjgzh:97a315342d0cd8229825ce300f608875b4e167122a792a3af40334d6e767c32b@ec2-44-206-204-65.compute-1.amazonaws.com:5432/d72b7vq7jdp4i3',
     entities: ['dist/**/*.entity{.ts,.js}'],
     synchronize: false,
     migrations: ['dist/db/migrations/*.js'],
